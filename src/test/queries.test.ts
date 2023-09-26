b4c9ab1e309ab8542799c5e395a9fbe25cdf7b4d
Javier Bullrich <javier@bullrich.dev>
2023-09-26 17:14:26 +0200

    fixed minor silly problem with the linter

--- src/test/queries.test.ts
+++ src/test/queries.test.ts
@@ -1,14 +1,13 @@
 import { validate } from "@octokit/graphql-schema";
 
-import { ENABLE_AUTO_MERGE, DISABLE_AUTO_MERGE } from "../github/merger";
+import { DISABLE_AUTO_MERGE, ENABLE_AUTO_MERGE } from "../github/merger";
 
 describe("Schemas", () => {
+  test("ENABLE_AUTO_MERGE", () => {
+    expect(validate(ENABLE_AUTO_MERGE)).toEqual([]);
+  });
 
-    test("ENABLE_AUTO_MERGE", () => {
-        expect(validate(ENABLE_AUTO_MERGE)).toEqual([]);
-    });
-
-    test("DISABLE_AUTO_MERGE", () => {
-        expect(validate(DISABLE_AUTO_MERGE)).toEqual([]);
-    });
+  test("DISABLE_AUTO_MERGE", () => {
+    expect(validate(DISABLE_AUTO_MERGE)).toEqual([]);
+  });
 });
