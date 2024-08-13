8e1e052f916a6bd8e56f94f1329bca1d56c4c277
Javier Bullrich <javier@bullrich.dev>
2024-08-13 10:49:27 +0200

    added auto-generation of graphql types

---
+++ scripts/copy-files.js
@@ -0,0 +1,26 @@
+const { readdirSync, writeFileSync, readFileSync } = require("fs");
+
+const files = readdirSync("src/github/graphql/");
+
+/**
+ * Copy a file and replace it's extension
+ * @param {string} fileName Name of the file to copy
+ * @param {string} extension New extension to put
+ */
+const copyFile = (fileName, extension) => {
+  console.log("Copying content of %s into a .ts file", fileName);
+  const content = readFileSync(fileName);
+  const oldExtension = fileName.split(".").pop();
+  writeFileSync(
+    fileName.replace(oldExtension, extension),
+    `// Generated from ${fileName}\nexport default \`${content}\`;`,
+  );
+};
+
+for (const file of files) {
+  if (file.endsWith(".graphql")) {
+    copyFile(`src/github/queries/${file}`, "ts");
+  }
+}
+
+copyFile("src/render/template.html", "ts");
