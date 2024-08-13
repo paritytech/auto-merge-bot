59d9c9b80d373ee71dc091c24ab6919fcd21bda2
Javier Bullrich <javier@bullrich.dev>
2024-08-13 11:23:18 +0200

    added query to update pull request

--- src/test/queries.test.ts
+++ src/test/queries.test.ts
@@ -3,6 +3,7 @@ import { validate } from "@octokit/graphql-schema";
 import DISABLE_AUTO_MERGE from "../github/graphql/DisableAutoMerge";
 import ENABLE_AUTO_MERGE from "../github/graphql/EnableAutoMerge";
 import MERGE_PULL_REQUEST from "../github/graphql/MergePullRequest";
+import UPDATE_PULL_REQUEST from "../github/graphql/UpdatePullRequest";
 
 describe("Schemas", () => {
   test("ENABLE_AUTO_MERGE", () => {
@@ -15,5 +16,9 @@ describe("Schemas", () => {
 
   test("MERGE_PULL_REQUEST", () => {
     expect(validate(MERGE_PULL_REQUEST)).toEqual([]);
+  });
+
+  test("UPDATE_PULL_REQUEST", () => {
+    expect(validate(UPDATE_PULL_REQUEST)).toEqual([]);
   });
 });
